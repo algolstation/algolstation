a5b658fd7af7f7403d796acb679fc24b4ced8694
Kapu1178 <75460809+Kapu1178@users.noreply.github.com>
2022-01-27 21:07:42 -0500

    I hate checkpaths

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -34,7 +34,7 @@ exactly 117 "to_world uses" '\sto_world\('
 exactly 53 "to_world_log uses" '\sto_world_log\('
 exactly 0 "world<< uses" 'world<<|world[[:space:]]<<'
 exactly 0 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
-exactly 2 "<< uses" '(?<!<)<<(?!<)' -P
+exactly 9 "<< uses" '(?<!<)<<(?!<)' -P
 exactly 2 ">> uses" '(?<!>)>>(?!>)' -P
 exactly 0 "incorrect indentations" '^( {4,})' -P
 exactly 23 "text2path uses" 'text2path'
